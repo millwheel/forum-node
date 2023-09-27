3d165cc81c2fd1437d92f93508f1f61bcb84210d
season <sionwer4@naver.com>
2023-09-27 22:21:11 +0900

    feat(main): Refactor post service to use dynamoose for post data CRUD

--- main/src/post/post.service.ts
+++ main/src/post/post.service.ts
@@ -1,47 +1,44 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Post } from './entity/post.entity';
 import { CreatePostDto, UpdatePostDto } from './dto/post.dto';
+import { PostModel } from './entity/post.model';
 
 @Injectable()
 export class PostService {
-  private posts: Post[] = [];
-
-  getAll(): Post[] {
-    return this.posts;
-  }
-
-  getOne(postId: number): Post {
-    const post = this.posts.find((post) => post.postId === postId);
-    return post;
-  }
-
-  create(createPostDto: CreatePostDto): Post {
-    const post = new Post();
-    post.postId = this.posts.length + 1;
-    post.writerId = createPostDto.userId;
-    post.title = createPostDto.title;
-    post.content = createPostDto.content;
-    post.tagList = createPostDto.tagList;
-    this.posts.push(post);
-    return post;
-  }
-
-  update(postId: number, updatePostDto: UpdatePostDto): Post {
-    const post = this.getOne(postId);
-    const propertiesToUpdate = ['title', 'content', 'tagList'];
-    propertiesToUpdate.forEach((prop) => {
-      if (updatePostDto[prop] !== undefined) {
-        post[prop] = updatePostDto[prop];
-      }
+  async getAll() {
+    const postInstances = await PostModel.scan().exec();
+    const responsePostDtos = new Array();
+    postInstances.map((post) => {
+      const { postId, writerId, title, content, tagList } = post.toJSON();
+      responsePostDtos.push({ postId, writerId, title, content, tagList });
     });
-    return post;
+    return responsePostDtos;
   }
 
-  deleteOne(postId: number): void {
-    const postIndex = this.posts.findIndex((post) => post.postId === postId);
-    if (postIndex === -1) {
-      throw new NotFoundException(`User with Id: ${postId} not found.`);
-    }
-    this.posts.splice(postIndex, 1);
+  async getOne(postId: number) {
+    const postInstance = await PostModel.get(postId);
+    const { writerId, title, content, tagList } = postInstance.toJSON();
+    return { postId, writerId, title, content, tagList };
+  }
+
+  async create(createPostDto: CreatePostDto) {
+    const postInstance = await PostModel.create({
+      postId: createPostDto.postId,
+      writerId: createPostDto.userId,
+      title: createPostDto.title,
+      content: createPostDto.content,
+      tagList: createPostDto.tagList,
+    });
+    const { postId, writerId, title, content, tagList } = postInstance.toJSON();
+    return { postId, writerId, title, content, tagList };
+  }
+
+  async update(postId: number, updatePostDto: UpdatePostDto) {
+    const postInstance = await PostModel.update({ postId }, updatePostDto);
+    const { writerId, title, content, tagList } = postInstance.toJSON();
+    return { postId, writerId, title, content, tagList };
+  }
+
+  async deleteOne(postId: number) {
+    await PostModel.delete(postId);
   }
 }
