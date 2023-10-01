b15cfd74dd3029cb9e8b473245c29edd8e8eaa9a
season <sionwer4@naver.com>
2023-10-01 15:13:35 +0900

    fix(message): Add kafka module to app module

--- message/src/app.module.ts
+++ message/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DynamooseModule } from './dynamoose/dynamoose.module';
+import { KafkaModule } from './kafka/kafka.module';
 
 @Module({
-  imports: [DynamooseModule],
+  imports: [DynamooseModule, KafkaModule],
   controllers: [AppController],
   providers: [AppService],
 })
