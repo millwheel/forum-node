b0f55d28f1eaab1b144ee76f7716b362e5b71c18
season <sionwer4@naver.com>
2023-10-02 18:58:42 +0900

    feat(message): Create redis configuartion

--- message/src/redis/redis.module.ts
+++ message/src/redis/redis.module.ts
@@ -1,20 +1,10 @@
 import { Global, Module, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import Redis from 'ioredis';
+import { RedisService } from './redis.service';
 
 @Global()
-@Module({})
-export class RedisModule implements OnModuleInit {
-  private redisClient: Redis;
-
-  constructor(private configService: ConfigService) {}
-
-  onModuleInit() {
-    const host = this.configService.get<string>('REDIS_HOST');
-    const port = this.configService.get<number>('REDIS_PORT');
-    const username = this.configService.get<string>('REDIS_ACCESS_KEY');
-    const password = this.configService.get<string>('REDIS_SECRET_KEY');
-
-    this.redisClient = new Redis({ host, port, username, password });
-  }
-}
+@Module({
+  providers: [RedisService, ConfigService],
+  exports: [RedisService],
+})
+export class RedisModule {}
